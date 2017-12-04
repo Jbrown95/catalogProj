dccdf2afb67f7646ce6f97339f4328476f3c2e87
Joshua Brown <joshuadbrown95@gmail.com>
2017-12-04 13:14:30 -0500

    pep8'ed

--- application.py
+++ application.py
@@ -1,18 +1,15 @@
-from flask import Flask,render_template,redirect,url_for,request,abort, g
-from flask import session as login_session
-from flask_login import login_manager,LoginManager,login_user,logout_user,login_required
-from urlparse import urlparse, urljoin
-
-
-
-
-# database modules
-from models import Base, Item, Category,User
+from urlparse import urlparse,  urljoin
+from models import Base,  Item,  Category, User
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship,  sessionmaker
 from sqlalchemy import create_engine
+from flask import Flask, render_template, redirect, url_for, request, abort,  g
+from flask import session as login_session
+from flask_login import login_manager, LoginManager, login_user, logout_user, \
+    login_required
 
-engine = create_engine('sqlite:///catalog.db',convert_unicode=True)
+
+engine = create_engine('sqlite:///catalog.db', convert_unicode=True)
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -25,34 +22,34 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 
-
 @login_manager.user_loader
 def load_user(user_id):
     try:
-        user = session.query(User).filter_by(user_id = user_id).first()
+        user = session.query(User).filter_by(user_id=user_id).first()
     except Exception:
         user = None
     return user
 
+
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-           ref_url.netloc == test_url.netloc
+    test_url = urlparse(urljoin(request.host_url,  target))
+    return test_url.scheme in ('http',  'https') and \
+        ref_url.netloc == test_url.netloc
 
 
 @app.route('/')
 def home():
     category = session.query(Category).all()
     items = session.query(Item).order_by(Item.created_date.desc()).all()
-    return render_template('home.html',category=category,items=items)
+    return render_template('home.html', category=category, items=items)
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         next = request.args.get('next')
-        return render_template('login.html',next = next)
+        return render_template('login.html', next=next)
     elif request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -60,7 +57,7 @@ def login():
         if username == '' or password == '':
             return redirect(url_for('login'))
         else:
-            user = session.query(User).filter_by(username = username).first()
+            user = session.query(User).filter_by(username=username).first()
             print(type(user))
             if not user or not user.verify_password(password):
                 return redirect(url_for('login'))
@@ -74,7 +71,6 @@ def login():
                 return redirect(next)
 
 
-
 @app.route("/logout")
 @login_required
 def logout():
@@ -82,20 +78,21 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/new_user',methods=['GET','POST'])
+@app.route('/new_user', methods=['GET', 'POST'])
 def new_user():
-    if request.method =='GET':
+    if request.method == 'GET':
         users = session.query(User).all()
-        return render_template('newuser.html',users=users)
+        return render_template('newuser.html', users=users)
 
-    elif request.method =='POST':
+    elif request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         if username == '' or password == '':
             return redirect(url_for('new_user'))
-        elif session.query(User).filter_by(username=username).first() is not None:
+        elif session.query(User).filter_by(username=username).first() \
+                is not None:
             return redirect(url_for('new_user'))
-        user = User(username = username,user_id=username)
+        user = User(username=username, user_id=username)
         user.hash_password(password)
         session.add(user)
         session.commit()
@@ -106,28 +103,32 @@ def new_user():
 @app.route('/catalog/<string:category_name>/items')
 def category(category_name):
     try:
-        category_name = session.query(Category).filter_by(name = category_name).one()
-        items = session.query(Item).filter_by(category_id=category_name.id).all()
-        return render_template('category.html',category_name=category_name,items=items)
+        category_name = session.query(Category).\
+            filter_by(name=category_name).one()
+        items = session.query(Item).filter_by(category_id=category_name.id).\
+            all()
+        return render_template('category.html',
+                               category_name=category_name, items=items)
     except:
         return redirect(url_for('home'))
 
+
 @app.route('/catalog/<string:category_name>/<string:item_name>')
-def catalogItem(category_name,item_name):
+def catalogItem(category_name, item_name):
     return "this is the the category_item Page"
 
 
-@app.route('/catalog/add/category',methods=['GET','POST'])
+@app.route('/catalog/add/category', methods=['GET', 'POST'])
 @login_required
 def addCategory():
     if request.method == 'POST':
         if request.form['name']:
             name = request.form['name']
             try:
-                session.query(Category).filter_by(name = name).one()
+                session.query(Category).filter_by(name=name).one()
                 return render_template('addCat.html')
             except:
-                category = Category(name = name)
+                category = Category(name=name)
                 session.add(category)
                 session.commit()
                 return redirect(url_for('home'))
@@ -138,18 +139,21 @@ def addCategory():
         return render_template('addCat.html')
 
 
-@app.route('/catalog/delete/category/<string:category_name>',methods=['GET','POST'])
+@app.route('/catalog/delete/category/<string:category_name>',
+           methods=['GET', 'POST'])
 @login_required
 def deleteCategory(category_name):
     if request.method == 'GET':
         try:
-            session.query(Category).filter_by(name = category_name).one()
-            return render_template('deleteCat.html',name = category_name)
+            session.query(Category).filter_by(name=category_name).one()
+            return render_template('deleteCat.html', name=category_name)
         except:
             return redirect(url_for('home'))
     elif request.method == 'POST':
-        category_to_delete = session.query(Category).filter_by(name = category_name).one()
-        items = session.query(Item).filter_by(category_id=category_to_delete.id).all()
+        category_to_delete = session.query(Category)\
+            .filter_by(name=category_name).one()
+        items = session.query(Item)\
+            .filter_by(category_id=category_to_delete.id).all()
         for i in items:
             session.delete(i)
         session.delete(category_to_delete)
@@ -157,64 +161,62 @@ def deleteCategory(category_name):
         return redirect(url_for('home'))
 
 
-@app.route('/catalog/add/item',methods=['GET','POST'])
+@app.route('/catalog/add/item', methods=['GET', 'POST'])
 @login_required
 def addItem():
     if request.method == 'GET':
         categories = session.query(Category).all()
-        return render_template('addItem.html',categories=categories)
+        return render_template('addItem.html', categories=categories)
     elif request.method == 'POST':
         categories = session.query(Category).all()
         try:
             item_name = request.form['name']
             item_description = request.form['description']
             item_category = request.form['category']
-            item_to_add = Item(item_name=item_name,description=item_description,category_id=item_category)
+            item_to_add = Item(item_name=item_name,
+                               description=item_description,
+                               category_id=item_category)
             print(1)
         except:
-            return render_template('addItem.html',categories=categories)
+            return render_template('addItem.html', categories=categories)
         try:
             session.query(Item).filter_by(item_name=item_name).one()
-            return render_template('addItem.html',categories=categories)
+            return render_template('addItem.html', categories=categories)
 
         except:
             if item_name == '' or item_description == '':
-                return render_template('addItem.html',categories=categories)
+                return render_template('addItem.html', categories=categories)
             else:
-                print(2)
-                print (item_category)
-                try :
+                try:
                     session.query(Category).filter_by(id=item_category).one()
-                    print (item_category)
                     session.add(item_to_add)
                     session.commit()
                     return redirect(url_for('home'))
                 except:
-                    return render_template('addItem.html',categories=categories)
-
-
+                    return render_template('addItem.html',
+                                           categories=categories)
 
 
 @app.route('/catalog/delete/<string:item_name>/')
 @login_required
 def deleteItem(item_name):
     if request.method == 'GET':
-        item_to_delete = session.query(Item).filter_by(item_name=item_name).one()
+        item_to_delete = session.query(Item)\
+            .filter_by(item_name=item_name).one()
         session.delete(item_to_delete)
         session.commit()
         return redirect(url_for('home'))
 
 
-
-@app.route('/catalog/<string:item_name>/description',methods=['GET','POST'])
+@app.route('/catalog/<string:item_name>/description', methods=['GET', 'POST'])
 def itemDescription(item_name):
     if request.method == 'GET':
         item = session.query(Item).filter_by(item_name=item_name).one()
-        return render_template('itemDescription.html',item=item)
+        return render_template('itemDescription.html', item=item)
     elif request.method == 'POST':
         return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.secret_key = "super_secret_key"
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0',  port=5000)
--- models.py
+++ models.py
@@ -1,22 +1,24 @@
-from sqlalchemy import Column,Integer,String,ForeignKey,DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship,  sessionmaker
 from sqlalchemy import create_engine
 import datetime
-import random,string
+import random
+import string
 from passlib.apps import custom_app_context as pwd_context
-from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,BadSignature, SignatureExpired)
-
-
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                          BadSignature,  SignatureExpired)
 
 
 Base = declarative_base()
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits)for x in xrange(32))
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits)for
+                     x in xrange(32))
+
 
 class User(Base):
     __tablename__ = 'user'
-    user_id = Column(String(32), primary_key=True)
-    username = Column(String(32), index=True)
+    user_id = Column(String(32),  primary_key=True)
+    username = Column(String(32),  index=True)
     password_hash = Column(String(64))
 
     # methods to support login manager
@@ -32,14 +34,14 @@ class User(Base):
     def get_id(self):
         return self.user_id
 
-    def hash_password(self, password):
+    def hash_password(self,  password):
         self.password_hash = pwd_context.encrypt(password)
 
-    def verify_password(self, password):
-        return pwd_context.verify(password,self.password_hash)
+    def verify_password(self,  password):
+        return pwd_context.verify(password, self.password_hash)
 
-    # def generate_auth_token(self,expiration=600):
-    #     s = Serializer(secret_key,expires_in = expiration)
+    # def generate_auth_token(self, expiration=600):
+    #     s = Serializer(secret_key, expires_in = expiration)
     #     print s.dumps({ 'id':self.id })
     #     return s.dumps({ 'id':self.id })
 
@@ -49,10 +51,10 @@ class User(Base):
         try:
             data = s.loads(token)
         except SignatureExpired:
-            #valid but expired
+            # valid but expired
             return None
         except BadSignature:
-            #BadSignature
+            # BadSignature
             return None
         user_id = data['id']
         return user_id
@@ -60,37 +62,39 @@ class User(Base):
 
 class Category(Base):
     __tablename__ = 'category'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer,  primary_key=True)
     name = Column(String)
+
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-        'id' : self.id,
-        'name' : self.item_name,
-        'description' : self.description,
-        'category' : self.category,
-            }
+                'id': self.id,
+                'name': self.item_name,
+                'description': self.description,
+                'category': self.category,
+                }
+
 
 class Item(Base):
     __tablename__ = 'item'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer,  primary_key=True)
     item_name = Column(String)
     description = Column(String)
-    created_date = Column(DateTime, default=datetime.datetime.utcnow)
-    category_id = Column(Integer, ForeignKey('category.id'))
+    created_date = Column(DateTime,  default=datetime.datetime.utcnow)
+    category_id = Column(Integer,  ForeignKey('category.id'))
     category = relationship(Category)
+
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-        'id' : self.id,
-        'name' : self.item_name,
-        'description' : self.description,
-        'category' : self.category,
-            }
-
-engine = create_engine('sqlite:///catalog.db',convert_unicode=True)
+                'id': self.id,
+                'name': self.item_name,
+                'description': self.description,
+                'category': self.category,
+                }
 
 
+engine = create_engine('sqlite:///catalog.db', convert_unicode=True)
 Base.metadata.create_all(engine)
